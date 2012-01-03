9c341f446a83381aa179da9e2fbc359d73b43e77
Jaime Alvarez <jalvarez@lablasamericas.com.co>
2012-01-03 10:32:24 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var App = new Ext.Application({
+	name : 'NotesApp',
+	useLoadMask : true,
+	launch : function() {
+		Ext.dispatch({
+			controller : NotesApp.controllers.notesController,
+			action : 'index' q
+		});
+	}
+});
